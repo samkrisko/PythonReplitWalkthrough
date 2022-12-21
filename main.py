91baa99b300c661a6ab6b49b25b5c0bc4487f2fa
Skrisko <121193587+samkrisko@users.noreply.github.com>
2022-12-21 20:07:57 +0000

    errythang

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from replit import db
+import os
+
+# store my name in the replit db
+# db["Name"] = 'sam'
+print(db['Name'])
+
+keys = db.keys()
+
+print(keys)
+
+for key in db:
+  print(db[key])
+
+
+
+
+
+
+
+
 def factorial(n):
   print(f'factorial({n})')
   if n == 0:
@@ -9,4 +30,7 @@ print(factorial(3))
 
 
 
+
+myApiKey = os.environ['apiKey']
+print(myApiKey)
 
